67ce2f1f0284a1281f6a92b9e5dfabf348581fda
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-07-05 13:56:53 -0700

    edit

---
+++ guess.js
@@ -0,0 +1,110 @@
+let randomNumber = parseInt((Math.random()*30)+1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const startOver = document.querySelector('.resultParas');
+const lowOrHi = document.querySelector('.lowOrHi');
+const errorMes = document.querySelector('.errorMes');
+const subt = document.querySelector('#subt');
+const p = document.createElement('p');
+let previousGuesses = [];
+let numGuesses = 1;
+let playGame = true;
+let numberFinal = 5;
+//var subth =document.getElementById('#subt');
+
+if (playGame){
+  subt.addEventListener('click', function(e){
+      e.preventDefault();
+      //User input
+      const guess = parseInt(userInput.value);
+      validateGuess(guess);
+  });
+}
+
+function validateGuess(guess){
+  if (isNaN(guess)){
+    displayError('<span id="red">Please enter a valid number</span>');
+  } else if (guess < 1) {
+    displayError(`Please enter a number greater than 1!`);
+  } else if (guess > 30){
+    displayError(`Please enter a number lower than 30!`);
+  } else {
+      //record of number of attempted gu
+      displayError('');
+            previousGuesses.push(guess);
+      //game over
+      if (numGuesses === numberFinal){
+          displayGuesses(guess);
+          displayMessage(`Sorry! The number was <span id="red">${randomNumber}</span>`);
+          endGame();
+      } else {
+      //Display guessed numbers
+      displayGuesses(guess);
+      //Check guess and display if wrong
+      checkGuess(guess);
+      }
+  }
+}
+
+function checkGuess(guess){
+  //show if guess is too high or too low
+  if (guess === randomNumber){
+      displayMessage('<span id="red">'+ guess + `</span> is Correct, Congratulations!`);
+      endGame();
+  } else if (guess < randomNumber) {
+      displayMessage(`<h2 id="hi">Higher!</h2>`);
+  } else if (guess > randomNumber) {
+      displayMessage(`<h2 id="lo">Lower!</h2>`);
+  }
+}
+
+function displayGuesses(guess){
+  userInput.value = '';
+  guessSlot.innerHTML += `<span id="gBubble">${guess}  </span>`;
+  numGuesses++;
+  remaining.innerHTML = `${numberFinal - (numGuesses)+1}  `;
+}
+
+function displayMessage(message){
+      lowOrHi.innerHTML = `<h2 id="hiLo">${message}</h2>`
+}
+
+function displayError(errorm) {
+  errorMes.innerHTML = `<span id="red">${errorm}</span>`
+}
+
+function endGame(){
+  //Clear user input
+  userInput.value = '';
+  //Disable user input button
+ subt.setAttribute('disabled', 'disabled');
+  // subth.classList.remove('guessSubmit', 'guessSubmit2');
+  // subth.classList.add('guessSubmit2');
+  userInput.setAttribute('disabled', '');
+  //Display Start new Game Button
+        p.classList.add('button');
+        p.innerHTML = `<h1 id="newGame">TRY AGAIN</h1>`
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
+}
+
+function newGame(){
+  const newGameButton = document.querySelector('#newGame');
+  newGameButton.addEventListener('click', function(){
+      //Starting a new game
+      randomNumber = parseInt((Math.random()*50)+1);
+      previousGuesses = [];
+      subth.classList.add('guessSubmit');
+      numGuesses = 1;
+      guessSlot.innerHTML = '';
+      lowOrHi.innerHTML = '';
+      remaining.innerHTML = `${numberFinal - numGuesses}  `;
+      userInput.removeAttribute('disabled');
+      startOver.removeChild(p);
+      playGame = true;
+  })
+}
