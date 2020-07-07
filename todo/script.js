a4f1052c529fd4ab5a9fe3f23f6b3115419a07df
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-07-06 19:43:58 -0700

    e

--- todo/script.js
+++ todo/script.js
@@ -1,11 +1,17 @@
 function todoList() {
-  var item = document.getElementById("todoInput").value
-  var text = document.createTextNode(item)
-  var newItem = document.createElement("li")
-  newItem.appendChild(text)
-  document.getElementById("todoList").appendChild(newItem)
+  var item = document.getElementById("todoInput").value;
+  var errorMes = document.querySelector(".errorMes");
+  if ( item === ''){
+      errorMes.innerHTML = `<span id="red">Please enter a task</span>`
+  }
+  else {
+    var text = document.createTextNode(item)
+    var newItem = document.createElement("li")
+    newItem.appendChild(text)
+    document.getElementById("todoList").appendChild(newItem)
 
-
-
+    document.getElementById("todoInput").value = '';
+    errorMes.innerHTML = '';
+    }
 
 }
